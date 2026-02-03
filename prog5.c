c005ba6fd36777bc0c964067bd829595eb74645f
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-02-03 18:23:13 +0530

    Add files via upload

---
+++ prog5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int missingNum(int arr[], int n) {
+    long long expectedSum = (long long)(n + 1) * (n + 2) / 2;
+    long long actualSum = 0;
+
+    for (int i = 0; i < n; i++) {
+        actualSum += arr[i];
+    }
+
+    return expectedSum - actualSum;
+}
+}
