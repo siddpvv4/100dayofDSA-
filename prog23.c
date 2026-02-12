35c4e85d25a4ccab1d8fa437d7d4ab4cb14b9f53
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-02-12 11:17:41 +0530

    Toeplitz Matrix

---
+++ prog23.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int matrix[20][20];
+
+    // input matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // check Toeplitz property
+    for(int i = 1; i < m; i++) {
+        for(int j = 1; j < n; j++) {
+
+            if(matrix[i][j] != matrix[i-1][j-1]) {
+                printf("false");
+                return 0;
+            }
+
+        }
+    }
+
+    printf("true");
+    return 0;
+}
