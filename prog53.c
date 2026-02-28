43cc8888543dae1fffc25945024d36a4f620cf25
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-02-28 11:08:56 +0530

    Circular Linked List Creation and Traversal

---
+++ prog53.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure for Node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create circular linked list
+struct Node* createCircularList(int n) {
+    if (n <= 0) return NULL;
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Make it circular
+    temp->next = head;
+
+    return head;
+}
+
+// Function to traverse circular linked list
+void traverseCircularList(struct Node* head) {
+    if (head == NULL) return;
+
+    struct Node* temp = head;
+
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* head = createCircularList(n);
+    traverseCircularList(head);
+
+    return 0;
+}
