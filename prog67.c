88fb5e5fd3294080220b39fcbd0e438680a8faa3
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-03-07 14:41:15 +0530

    Queue Using Array

---
+++ prog67.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int queue[MAX];
+int front = -1;
+int rear = -1;
+
+void enqueue(int value) {
+    if (rear == MAX - 1) {
+        printf("Queue Overflow\n");
+        return;
+    }
+
+    if (front == -1)
+        front = 0;
+
+    rear++;
+    queue[rear] = value;
+}
+
+void display() {
+    if (front == -1) {
+        printf("Queue is Empty");
+        return;
+    }
+
+    for (int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+}
+
+int main() {
+    int n, x;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    display();
+
+    return 0;
+}
