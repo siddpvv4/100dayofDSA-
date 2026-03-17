0310031eec54492f8243deb9e0c2a0909dc3120a
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-03-17 12:03:30 +0530

    Maximum Depth of Binary Tree

---
+++ prog88.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Create new node
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Build tree from level order
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct TreeNode** queue = (struct TreeNode**)malloc(n * sizeof(struct TreeNode*));
+    int front = 0, rear = 0;
+
+    struct TreeNode* root = newNode(arr[0]);
+    queue[rear++] = root;
+
+    int i = 1;
+
+    while (i < n) {
+        struct TreeNode* current = queue[front++];
+
+        // Left child
+        if (i < n && arr[i] != -1) {
+            current->left = newNode(arr[i]);
+            queue[rear++] = current->left;
+        }
+        i++;
+
+        // Right child
+        if (i < n && arr[i] != -1) {
+            current->right = newNode(arr[i]);
+            queue[rear++] = current->right;
+        }
+        i++;
+    }
+
+    free(queue);
+    return root;
+}
+
+// Max Depth function
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return 1 + (left > right ? left : right);
+}
+
+// Main function
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct TreeNode* root = buildTree(arr, n);
+
+    int depth = maxDepth(root);
+    printf("%d\n", depth);
+
+    return 0;
+}
