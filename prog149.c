3b38d181302de2d73bb59f0f3b40f22e47a93bb7
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-04-18 12:44:46 +0530

    Check if Graph is Connected

---
+++ prog149.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+int visited[MAX];
+int adj[MAX][MAX];  // adjacency matrix
+
+void dfs(int node, int n) {
+    visited[node] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            dfs(i, n);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    // initialize adjacency matrix
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+        visited[i] = 0;
+    }
+
+    // input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // undirected graph
+    }
+
+    // start DFS from node 1
+    dfs(1, n);
+
+    // check if all nodes are visited
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED\n");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED\n");
+    return 0;
+}
