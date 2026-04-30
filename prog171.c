ccbddae21ed28e0e5984125fe2045a946dcc8944
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-04-30 23:23:10 +0530

    Implement merge sort to count inversions in array

---
+++ prog171.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+long long merge(int arr[], int temp[], int left, int mid, int right) {
+    int i = left;     // left subarray
+    int j = mid + 1;  // right subarray
+    int k = left;
+    long long inv_count = 0;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+            inv_count += (mid - i + 1);  // key line
+        }
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+
+    return inv_count;
+}
+
+long long mergeSort(int arr[], int temp[], int left, int right) {
+    long long inv_count = 0;
+
+    if (left < right) {
+        int mid = (left + right) / 2;
+
+        inv_count += mergeSort(arr, temp, left, mid);
+        inv_count += mergeSort(arr, temp, mid + 1, right);
+        inv_count += merge(arr, temp, left, mid, right);
+    }
+
+    return inv_count;
+}
+
+long long countInversions(int arr[], int n) {
+    int temp[n];
+    return mergeSort(arr, temp, 0, n - 1);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("%lld\n", countInversions(arr, n));
+
+    return 0;
+}
